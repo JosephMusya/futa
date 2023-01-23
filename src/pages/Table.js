aa13a396d9d4be548e73ad2e1f3596f426b27222
Joseph Musya <josephmusya254@gmail.com>
2023-01-23 11:22:17 +0300

    Edited the tabledisplay

--- src/pages/Table.js
+++ src/pages/Table.js
@@ -1,13 +1,313 @@
 import styles from './Table.module.css'
 import {useState,useEffect} from 'react';
 
-function Table(props) {
-    console.log(props)
+function Table(props) {    
     const currentSeason = String(props.season)
     // const apiKey = '89a5bb67ca02563cd394a66791f47168';
     const apiKey = 'cecdb0e87445150864d079cd5c982aa5'
     const [loading,setLoading] = useState(true)
     const [loadedData, setLoadedData] = useState([])
+
+    const rawData = {
+      "get": "standings",
+      "parameters": {
+        "league": "39",
+        "season": "2019"
+      },
+      "errors": [],
+      "results": 1,
+      "paging": {
+        "current": 1,
+        "total": 1
+      },
+      "response": [
+        {
+          "league": {
+            "id": 39,
+            "name": "Premier League",
+            "country": "England",
+            "logo": "https://media.api-sports.io/football/leagues/2.png",
+            "flag": "https://media.api-sports.io/flags/gb.svg",
+            "season": 2019,
+            "standings": [
+              [
+                {
+                  "rank": 1,
+                  "team": {
+                    "id": 40,
+                    "name": "Liverpool",
+                    "logo": "https://media.api-sports.io/football/teams/40.png"
+                  },
+                  "points": 70,
+                  "goalsDiff": 41,
+                  "group": "Premier League",
+                  "form": "WWWWW",
+                  "status": "same",
+                  "description": "Promotion - Champions League (Group Stage)",
+                  "all": {
+                    "played": 24,
+                    "win": 23,
+                    "draw": 1,
+                    "lose": 0,
+                    "goals": {
+                      "for": 56,
+                      "against": 15
+                    }
+                  },
+                  "home": {
+                    "played": 12,
+                    "win": 12,
+                    "draw": 0,
+                    "lose": 0,
+                    "goals": {
+                      "for": 31,
+                      "against": 9
+                    }
+                  },
+                  "away": {
+                    "played": 12,
+                    "win": 11,
+                    "draw": 1,
+                    "lose": 0,
+                    "goals": {
+                      "for": 25,
+                      "against": 6
+                    }
+                  },
+                  "update": "2020-01-29T00:00:00+00:00"
+                },
+                {
+                  "rank": 1,
+                  "team": {
+                    "id": 40,
+                    "name": "Liverpool",
+                    "logo": "https://media.api-sports.io/football/teams/40.png"
+                  },
+                  "points": 70,
+                  "goalsDiff": 41,
+                  "group": "Premier League",
+                  "form": "WWWWW",
+                  "status": "same",
+                  "description": "Promotion - Champions League (Group Stage)",
+                  "all": {
+                    "played": 24,
+                    "win": 23,
+                    "draw": 1,
+                    "lose": 0,
+                    "goals": {
+                      "for": 56,
+                      "against": 15
+                    }
+                  },
+                  "home": {
+                    "played": 12,
+                    "win": 12,
+                    "draw": 0,
+                    "lose": 0,
+                    "goals": {
+                      "for": 31,
+                      "against": 9
+                    }
+                  },
+                  "away": {
+                    "played": 12,
+                    "win": 11,
+                    "draw": 1,
+                    "lose": 0,
+                    "goals": {
+                      "for": 25,
+                      "against": 6
+                    }
+                  },
+                  "update": "2020-01-29T00:00:00+00:00"
+                },
+                {
+                  "rank": 1,
+                  "team": {
+                    "id": 40,
+                    "name": "Liverpool",
+                    "logo": "https://media.api-sports.io/football/teams/40.png"
+                  },
+                  "points": 70,
+                  "goalsDiff": 41,
+                  "group": "Premier League",
+                  "form": "WWWWW",
+                  "status": "same",
+                  "description": "Promotion - Champions League (Group Stage)",
+                  "all": {
+                    "played": 24,
+                    "win": 23,
+                    "draw": 1,
+                    "lose": 0,
+                    "goals": {
+                      "for": 56,
+                      "against": 15
+                    }
+                  },
+                  "home": {
+                    "played": 12,
+                    "win": 12,
+                    "draw": 0,
+                    "lose": 0,
+                    "goals": {
+                      "for": 31,
+                      "against": 9
+                    }
+                  },
+                  "away": {
+                    "played": 12,
+                    "win": 11,
+                    "draw": 1,
+                    "lose": 0,
+                    "goals": {
+                      "for": 25,
+                      "against": 6
+                    }
+                  },
+                  "update": "2020-01-29T00:00:00+00:00"
+                },
+                {
+                  "rank": 1,
+                  "team": {
+                    "id": 40,
+                    "name": "Liverpool",
+                    "logo": "https://media.api-sports.io/football/teams/40.png"
+                  },
+                  "points": 70,
+                  "goalsDiff": 41,
+                  "group": "Premier League",
+                  "form": "WWWWW",
+                  "status": "same",
+                  "description": "Promotion - Champions League (Group Stage)",
+                  "all": {
+                    "played": 24,
+                    "win": 23,
+                    "draw": 1,
+                    "lose": 0,
+                    "goals": {
+                      "for": 56,
+                      "against": 15
+                    }
+                  },
+                  "home": {
+                    "played": 12,
+                    "win": 12,
+                    "draw": 0,
+                    "lose": 0,
+                    "goals": {
+                      "for": 31,
+                      "against": 9
+                    }
+                  },
+                  "away": {
+                    "played": 12,
+                    "win": 11,
+                    "draw": 1,
+                    "lose": 0,
+                    "goals": {
+                      "for": 25,
+                      "against": 6
+                    }
+                  },
+                  "update": "2020-01-29T00:00:00+00:00"
+                },
+                {
+                  "rank": 1,
+                  "team": {
+                    "id": 40,
+                    "name": "Liverpool",
+                    "logo": "https://media.api-sports.io/football/teams/40.png"
+                  },
+                  "points": 70,
+                  "goalsDiff": 41,
+                  "group": "Premier League",
+                  "form": "WWWWW",
+                  "status": "same",
+                  "description": "Promotion - Champions League (Group Stage)",
+                  "all": {
+                    "played": 24,
+                    "win": 23,
+                    "draw": 1,
+                    "lose": 0,
+                    "goals": {
+                      "for": 56,
+                      "against": 15
+                    }
+                  },
+                  "home": {
+                    "played": 12,
+                    "win": 12,
+                    "draw": 0,
+                    "lose": 0,
+                    "goals": {
+                      "for": 31,
+                      "against": 9
+                    }
+                  },
+                  "away": {
+                    "played": 12,
+                    "win": 11,
+                    "draw": 1,
+                    "lose": 0,
+                    "goals": {
+                      "for": 25,
+                      "against": 6
+                    }
+                  },
+                  "update": "2020-01-29T00:00:00+00:00"
+                },
+                {
+                  "rank": 1,
+                  "team": {
+                    "id": 40,
+                    "name": "Liverpool",
+                    "logo": "https://media.api-sports.io/football/teams/40.png"
+                  },
+                  "points": 70,
+                  "goalsDiff": 41,
+                  "group": "Premier League",
+                  "form": "WWWWW",
+                  "status": "same",
+                  "description": "Promotion - Champions League (Group Stage)",
+                  "all": {
+                    "played": 24,
+                    "win": 23,
+                    "draw": 1,
+                    "lose": 0,
+                    "goals": {
+                      "for": 56,
+                      "against": 15
+                    }
+                  },
+                  "home": {
+                    "played": 12,
+                    "win": 12,
+                    "draw": 0,
+                    "lose": 0,
+                    "goals": {
+                      "for": 31,
+                      "against": 9
+                    }
+                  },
+                  "away": {
+                    "played": 12,
+                    "win": 11,
+                    "draw": 1,
+                    "lose": 0,
+                    "goals": {
+                      "for": 25,
+                      "against": 6
+                    }
+                  },
+                  "update": "2020-01-29T00:00:00+00:00"
+                },
+              ]
+            ]
+          }
+        }
+      ]
+    }
+
     async function getSeason(){
       const res = await fetch(
         'https://v3.football.api-sports.io/standings?league=39&season='+currentSeason,
@@ -21,95 +321,23 @@ function Table(props) {
 
       const data = await res.json()
       const response = await data.response
-      return response
+      // return response
+      return rawData.response
     }    
     
     useEffect(()=>{
       if(currentSeason){
+        console.log("Getting standings...")
         getSeason().then(
         response=>{
           setLoadedData(response)
           setLoading(false)
+          console.log("Fetched data...")
           console.log(loadedData)
         }
       )
       }
-    },[currentSeason])
-    
-    // const data = {
-    //   "get": "standings",
-    //   "parameters": {
-    //     "league": "39",
-    //     "season": "2019"
-    //   },
-    //   "errors": [],
-    //   "results": 1,
-    //   "paging": {
-    //     "current": 1,
-    //     "total": 1
-    //   },
-    //   "response": [
-    //     {
-    //       "league": {
-    //         "id": 39,
-    //         "name": "Premier League",
-    //         "country": "England",
-    //         "logo": "https://media.api-sports.io/football/leagues/2.png",
-    //         "flag": "https://media.api-sports.io/flags/gb.svg",
-    //         "season": 2019,
-    //         "standings": [
-    //           [
-    //             {
-    //               "rank": 1,
-    //               "team": {
-    //                 "id": 40,
-    //                 "name": "Liverpool",
-    //                 "logo": "https://media.api-sports.io/football/teams/40.png"
-    //               },
-    //               "points": 70,
-    //               "goalsDiff": 41,
-    //               "group": "Premier League",
-    //               "form": "WWWWW",
-    //               "status": "same",
-    //               "description": "Promotion - Champions League (Group Stage)",
-    //               "all": {
-    //                 "played": 24,
-    //                 "win": 23,
-    //                 "draw": 1,
-    //                 "lose": 0,
-    //                 "goals": {
-    //                   "for": 56,
-    //                   "against": 15
-    //                 }
-    //               },
-    //               "home": {
-    //                 "played": 12,
-    //                 "win": 12,
-    //                 "draw": 0,
-    //                 "lose": 0,
-    //                 "goals": {
-    //                   "for": 31,
-    //                   "against": 9
-    //                 }
-    //               },
-    //               "away": {
-    //                 "played": 12,
-    //                 "win": 11,
-    //                 "draw": 1,
-    //                 "lose": 0,
-    //                 "goals": {
-    //                   "for": 25,
-    //                   "against": 6
-    //                 }
-    //               },
-    //               "update": "2020-01-29T00:00:00+00:00"
-    //             }
-    //           ]
-    //         ]
-    //       }
-    //     }
-    //   ]
-    // }
+    },[currentSeason])      
 
     const standings = loadedData
 
