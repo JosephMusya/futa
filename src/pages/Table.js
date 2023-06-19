f503acbcb277d5723bc4ac300f69fe731118ccec
josephmusya <muciajoe@gmail.com>
2023-06-19 22:36:16 +0300

    Modified the display structure

--- src/pages/Table.js
+++ src/pages/Table.js
@@ -63,77 +63,79 @@ function Table() {
               standings.map((data,index)=>{
                 const teams = (data.league.standings);  
                 return (
-                  <div key={index} className={styles.tableDiv}>
-                    <table>
-                      <tr>
-                        <th>Team</th>
-                        <th>PTS</th>
-                        <th>P</th>
-                        <th>W</th>
-                        <th>D</th>
-                        <th>L</th>
-                        <th>GF</th>
-                        <th>GA</th>
-                        <th>GD</th>
-                        <th>Form</th>
-                      </tr>    
-                        {
-                        teams.map(team=>{  
-                          const trancatedTeams = team.slice(0,5)
-                                                   
-                          return (                       
-                            (trancated?trancatedTeams:team).map((team,index)=>{
-                              return (
-                              <tr key={index}>
-                                <td className={styles.teamName}>
-                                  <div>
-                                    <article className={styles.teamRank}>{team.rank}</article>
-                                    <img src={team.team.logo} alt={team.team.name} />
-                                    <Link to='/team-staticstics'>{team.team.name}</Link>                                     
-                                  </div>                                  
-                                </td>
-                                <td>{team.points}</td>
-                                <td>{team.all.played}</td>
-                                <td>{team.all.win}</td>
-                                <td>{team.all.draw}</td>
-                                <td>{team.all.lose}</td>
-                                <td>{team.all.goals.for}</td>
-                                <td>{team.all.goals.against}</td>
-                                <td>{team.goalsDiff}</td>  
-                                <td className={styles.form}>                            
-                                {
-                                  team.form?((team.form).split('')).map((form)=>{
-                                    if (form==='W'){
-                                      return <div>
-                                          <img className={styles.win} src={win} alt="Winning Icon" />
-                                        </div>
-                                    }
+                  <div key={index} className={styles.tableDiv}>                    
+                    <div className={styles.tableContent}>
+                      <table>
+                        <tr>
+                          <th>Team</th>
+                          <th>PTS</th>
+                          <th>P</th>
+                          <th>W</th>
+                          <th>D</th>
+                          <th>L</th>
+                          <th>GF</th>
+                          <th>GA</th>
+                          <th>GD</th>
+                          <th>Form</th>
+                        </tr>    
+                          {
+                          teams.map(team=>{  
+                            const trancatedTeams = team.slice(0,5)
+                                                    
+                            return (                       
+                              (trancated?trancatedTeams:team).map((team,index)=>{
+                                return (
+                                <tr key={index}>
+                                  <td className={styles.teamName}>
+                                    <div>
+                                      <article className={styles.teamRank}>{team.rank}</article>
+                                      <img src={team.team.logo} alt={team.team.name} />
+                                      <Link to='/team-staticstics'>{team.team.name}</Link>                                     
+                                    </div>                                  
+                                  </td>
+                                  <td>{team.points}</td>
+                                  <td>{team.all.played}</td>
+                                  <td>{team.all.win}</td>
+                                  <td>{team.all.draw}</td>
+                                  <td>{team.all.lose}</td>
+                                  <td>{team.all.goals.for}</td>
+                                  <td>{team.all.goals.against}</td>
+                                  <td>{team.goalsDiff}</td>  
+                                  <td className={styles.form}>                            
+                                  {
+                                    team.form?(team.form.split('')).map((form)=>{
+                                      if (form==='W'){
+                                        return <div>
+                                            <img className={styles.win} src={win} alt="Winning Icon" />
+                                          </div>
+                                      }
 
-                                    else if (form==='D'){
-                                      return <div>
-                                          <img className={styles.win} src={draw} alt="Winning Icon" />
-                                        </div>
-                                    }
+                                      else if (form==='D'){
+                                        return <div>
+                                            <img className={styles.win} src={draw} alt="Winning Icon" />
+                                          </div>
+                                      }
 
-                                    else if (form=='L'){
-                                      return <div>
-                                          <img className={styles.lost} src={lost} alt="Lost Icon" />
-                                        </div>
-                                    }
-                                  }):null
-                                }
-                                </td>
-                              </tr>
-                              )
-                            })
-                          )
-                        })                                                                 
-                      }                                        
-                    </table>
-                    {
-                      trancated ? <button onClick={trancateHandler}>More</button> : 
-                      <button onClick={trancateHandler}>Less</button>
-                    }
+                                      else if (form=='L'){
+                                        return <div>
+                                            <img className={styles.lost} src={lost} alt="Lost Icon" />
+                                          </div>
+                                      }
+                                    }):null
+                                  }
+                                  </td>
+                                </tr>
+                                )
+                              })
+                            )
+                          })                                                                 
+                        }                                        
+                      </table>
+                      {
+                        trancated ? <button onClick={trancateHandler}>More</button> : 
+                        <button onClick={trancateHandler}>Less</button>
+                      }
+                    </div>
                     <Statistics apiKey={apiKey} season={currentSeason}/>
                   </div>
                   )
